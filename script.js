4180cb39165574a3dd32b335f4e4c9bd349f8c66
djMischa <github@chiroyoga.ca>
2025-12-04 17:38:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -548,7 +548,7 @@ async function handleFullRegistration() {
         border:none;
         border-radius:8px;
         cursor:pointer;
-      ">Enter correct WhatsApp to continue</button>
+      ">Re-Submit WhatsApp</button>
     </div>
   `;
 
